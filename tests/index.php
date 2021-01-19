d7facc5b56d439476573d1d6ab583741a3762b03
chuunchun <2192138785@qq.com>
2021-01-19 22:02:11 +0800

    1.0.7

--- tests/index.php
+++ tests/index.php
@@ -14,7 +14,6 @@ defined('YII_ENV') or define('YII_ENV', getenv('PHP_ENV') === 'development' ? 'd
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
-
 $config = require __DIR__ . '/config/server.php';
 $server = new Server($config);
 $server->run();
