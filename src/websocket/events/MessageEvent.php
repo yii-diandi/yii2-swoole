26559fa082c1b1c57e9f0a9e5b2884b11d6ab8e8
chunchun <2192138785@qq.com>
2021-09-03 15:52:58 +0800

    2.1.6

--- src/websocket/events/MessageEvent.php
+++ src/websocket/events/MessageEvent.php
@@ -17,17 +17,3 @@ class MessageEvent extends Event
 {
     public $message;
 }
-
-class Mailer extends Component
-{
-    const EVENT_MESSAGE_SENT = 'messageSent';
-
-    public function send($message)
-    {
-        // ...发送 $message 的逻辑...
-
-        $event = new MessageEvent;
-        $event->message = $message;
-        $this->trigger(self::EVENT_MESSAGE_SENT, $event);
-    }
-}
